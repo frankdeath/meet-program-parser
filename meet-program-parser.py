49a2e979bba2fb364561881ff862f41b6a265a94
Kevin Peterson <frankdeath@gmail.com>
2025-07-02 15:50:55 -0500

    Parse header lines

--- meet-program-parser.py
+++ meet-program-parser.py
@@ -13,6 +13,7 @@ lane = None
 # This doesn't work for events that don't have age groups: "#1 Girls 200 Yard Freestyle"
 #!eventPattern = r"#(?P<eventNum>[0-9]+) (?P<gender>\w+) (?P<ageGroup>.+) (?P<distance>[0-9]+ \w+) (?P<stroke>.*)"
 eventPattern = r"#(?P<eventNum>[0-9]+) (?P<eventName>.+)"
+headerPattern = r"^ *(?P<header>Team|Relay|Lane|Seed Time|Age|Name)"
 
 for page in doc:
 	
@@ -20,11 +21,21 @@ for page in doc:
 	#print(text)
 	
 	for line in text.split("\n"):
-		if len(line) > 0 and line[0] == "#":
+		matches = re.findall(eventPattern, line)
+		if matches != []:
 			# Event line
+			#!print(matches)
 			#!print(line)
 			matches = re.findall(eventPattern, line)
 			#!print(matches)
 			eventNum, eventName = matches[0]
 			print(eventNum, eventName)
 			
+		matches = re.findall(headerPattern, line)
+		if matches != []:
+			# Header line
+			#!print(matches)
+			# Do nothing with this right now. Eventually collect the headers for an event to know what to display
+			pass
+		#!else:
+		#!	print("!", line)
