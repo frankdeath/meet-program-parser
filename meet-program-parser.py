2e877c7ffcca7c56a1c3ca18482bdeb5ed3e7cf6
Kevin Peterson <frankdeath@gmail.com>
2025-07-02 15:37:45 -0500

    Parse event lines

--- meet-program-parser.py
+++ meet-program-parser.py
@@ -5,9 +5,14 @@ import re
 
 doc = fitz.open("meet-program.pdf")
 
-event = None
+eventNum = None
+eventName = None
 heat = None
 lane = None
+
+# This doesn't work for events that don't have age groups: "#1 Girls 200 Yard Freestyle"
+#!eventPattern = r"#(?P<eventNum>[0-9]+) (?P<gender>\w+) (?P<ageGroup>.+) (?P<distance>[0-9]+ \w+) (?P<stroke>.*)"
+eventPattern = r"#(?P<eventNum>[0-9]+) (?P<eventName>.+)"
 
 for page in doc:
 	
@@ -16,5 +21,10 @@ for page in doc:
 	
 	for line in text.split("\n"):
 		if len(line) > 0 and line[0] == "#":
-			# event line
-			print(line)
+			# Event line
+			#!print(line)
+			matches = re.findall(eventPattern, line)
+			#!print(matches)
+			eventNum, eventName = matches[0]
+			print(eventNum, eventName)
+			
