2a299c7de1c77c53b94772daa0e70d22a38c9970
Kevin Peterson <frankdeath@gmail.com>
2025-07-02 15:57:31 -0500

    Parse heat lines

--- meet-program-parser.py
+++ meet-program-parser.py
@@ -7,13 +7,16 @@ doc = fitz.open("meet-program.pdf")
 
 eventNum = None
 eventName = None
-heat = None
+heatNum = None
+numHeats = None
+stage = None
 lane = None
 
 # This doesn't work for events that don't have age groups: "#1 Girls 200 Yard Freestyle"
 #!eventPattern = r"#(?P<eventNum>[0-9]+) (?P<gender>\w+) (?P<ageGroup>.+) (?P<distance>[0-9]+ \w+) (?P<stroke>.*)"
 eventPattern = r"#(?P<eventNum>[0-9]+) (?P<eventName>.+)"
 headerPattern = r"^ *(?P<header>Team|Relay|Lane|Seed Time|Age|Name)"
+heatPattern = r"Heat *(?P<heatNum>[0-9]+) of (?P<numHeats>[0-9]+) *(?P<stage>\w+)"
 
 for page in doc:
 	
@@ -37,5 +40,12 @@ for page in doc:
 			#!print(matches)
 			# Do nothing with this right now. Eventually collect the headers for an event to know what to display
 			pass
+		
+		matches = re.findall(heatPattern, line)
+		if matches != []:
+			# Heat line
+			#!print(matches)
+			heatNum, numHeats, stage = matches[0]
+			print(heatNum, numHeats)
 		#!else:
 		#!	print("!", line)
