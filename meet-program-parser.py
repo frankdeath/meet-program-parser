31b85d9564c36759582d6a5ae8237ada5545fe09
Kevin Peterson <frankdeath@gmail.com>
2025-07-03 11:11:12 -0500

    Get text in blocks.  Parses some of the races correctly, but there are still many bugs

--- meet-program-parser.py
+++ meet-program-parser.py
@@ -12,40 +12,100 @@ numHeats = None
 stage = None
 lane = None
 
+firstIndent = "  "
+secondIndent = "    "
+
 # This doesn't work for events that don't have age groups: "#1 Girls 200 Yard Freestyle"
 #!eventPattern = r"#(?P<eventNum>[0-9]+) (?P<gender>\w+) (?P<ageGroup>.+) (?P<distance>[0-9]+ \w+) (?P<stroke>.*)"
 eventPattern = r"#(?P<eventNum>[0-9]+) (?P<eventName>.+)"
-headerPattern = r"^ *(?P<header>Team|Relay|Lane|Seed Time|Age|Name)"
+headerPattern = r"(?P<header>Team|Relay|Lane|Seed Time|Age|Name)"
 heatPattern = r"Heat *(?P<heatNum>[0-9]+) of (?P<numHeats>[0-9]+) *(?P<stage>\w+)"
+relayNamePattern = r"(?P<name>[\w ,\.]+) (?P<age>[0-9]+)"
 
 for page in doc:
 	
-	text = page.get_text()
+	text = page.get_text("blocks")
 	#print(text)
 	
-	for line in text.split("\n"):
-		matches = re.findall(eventPattern, line)
+	for row in text:
+		
+		# A row is a tuple like this: (18.0, 82.0386734008789, 102.17560577392578, 90.0107421875, '#1 Girls 200 Yard Freestyle\n', 1, 0)
+		# We only care about the string at index 4
+		data = row[4]
+		
+		#!print(data)
+		#!print()
+		
+		
+		matches = re.findall(eventPattern, data)
 		if matches != []:
 			# Event line
 			#!print(matches)
 			#!print(line)
-			matches = re.findall(eventPattern, line)
+			matches = re.findall(eventPattern, data)
 			#!print(matches)
 			eventNum, eventName = matches[0]
-			print(eventNum, eventName)
-			
-		matches = re.findall(headerPattern, line)
-		if matches != []:
-			# Header line
-			#!print(matches)
-			# Do nothing with this right now. Eventually collect the headers for an event to know what to display
-			pass
-		
-		matches = re.findall(heatPattern, line)
-		if matches != []:
-			# Heat line
-			#!print(matches)
-			heatNum, numHeats, stage = matches[0]
-			print(heatNum, numHeats)
-		#!else:
-		#!	print("!", line)
+			print()
+			print("#{} - {}".format(eventNum, eventName))
+		else:
+			matches = re.findall(headerPattern, data)
+			if matches != []:
+				# Header line
+				#!print(data)
+				#!print(matches)
+				# Do nothing with this right now. Eventually collect the headers for an event to know what to display
+				pass
+			else:
+				matches = re.findall(heatPattern, data)
+				if matches != []:
+					# Heat line
+					#!print(data.replace("\n", "\\n"))
+					#!print(matches)
+					#!print()
+					fields = data.split("\n")
+					#!print(fields, len(fields))
+					
+					heatStr = fields[0]
+					matches = re.findall(heatPattern, heatStr)
+					heatNum, numHeats, stage = matches[0]
+					print()
+					print("Heat {} of {}".format(heatNum, numHeats))
+					
+					if len(fields) == 7:
+						# Relay individual line
+						team, seedTime, age, name, lane = fields[1:6]
+						print(firstIndent, lane, name, age, team, seedTime)
+					elif len(fields) == 6:
+						# For relays team = relay and name = team
+						relay, seedTime, team, lane = fields[1:5]
+						# Relay team line
+						print(firstIndent, lane, team, relay, seedTime)
+					else:
+						print("Error! Can't parse: {}".format(data))
+					
+				else:
+					matches = re.findall(relayNamePattern, data)
+					if matches != []:
+						# Relay name line
+						#!print(matches)
+						for match in matches:
+							name, age = match
+							print(secondIndent, name, age)
+						#!lastName, firstName, 
+					else:
+						#!print(data.replace("\n", "\\n"))
+						#!print(data.split('\n'))
+						fields = data.split('\n')
+						
+						if len(fields) == 5:
+							# Relay team line
+							relay, seedTime, team, lane = fields[:4]
+							print(firstIndent, lane, team, relay, seedTime)
+						elif len(fields) == 6:
+							# Individual line
+							team, seedTime, age, name, lane = fields[:5]
+							print(firstIndent, lane, name, age, team, seedTime)
+						else:
+							#!print(data.replace("\n", "\\n"))
+							print(data.split('\n'))
+				
